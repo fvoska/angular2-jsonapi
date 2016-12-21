fc6ddb57253fa52ae2bc4afd456a27e72d9b106f
Mihael Safaric <mihael.safaric@gmail.com>
2016-12-21 02:11:31 +0100

    test

--- src/services/json-api-datastore.service.ts
+++ src/services/json-api-datastore.service.ts
@@ -153,7 +153,7 @@ export class JsonApiDatastore {
     return model;
   }
 
-  protected handleError(error: any): ErrorObservable<any> {
+  protected handleError(error: any) {
     let errMsg: string = (error.message) ? error.message :
         error.status ? `${error.status} - ${error.statusText}` : 'Server error';
     try {
