c0c1d2777d1440755c1f3a4516ffef8e9169d96e
Mihael Safaric <mihael.safaric@gmail.com>
2017-02-24 22:04:48 +0100

    Generate build

--- dist/module.js
+++ dist/module.js
@@ -5,9 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var core_1 = require("@angular/core");
 var http_1 = require("@angular/http");
 var providers_1 = require("./providers");
@@ -20,8 +17,7 @@ JsonApiModule = __decorate([
     core_1.NgModule({
         providers: [providers_1.PROVIDERS],
         exports: [http_1.HttpModule]
-    }),
-    __metadata("design:paramtypes", [])
+    })
 ], JsonApiModule);
 exports.JsonApiModule = JsonApiModule;
 //# sourceMappingURL=module.js.map
